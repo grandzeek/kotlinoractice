71bc63d261ddfc13579e13cbcb88c8f058ba87f5
grandzeek <elsantocm7@gmail.com>
2023-05-09 10:12:24 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Hello, World!")
+    val num = 10
+
+    if (num > 0) {
+        println("The number is positive.")
+    } else if (num < 0) {
+        println("The number is negative.")
+    } else {
+        println("The number is zero.")
+    }
+
+}
+val age: Int = 27 //value cant be changed
+
+
